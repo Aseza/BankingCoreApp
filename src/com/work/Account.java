d67ffc1ddae62b15778b98b2b5272f39f6fd8227
Aseza <Abdel_96@live.fr>
2017-12-16 16:39:31 +0000

    Adding comments in Account.java

--- src/com/work/Account.java
+++ src/com/work/Account.java
@@ -3,13 +3,15 @@ package com.work;
 public class Account {
 	private double balance;
 
+	//Method to add Balance, referred in Client
 	public void addBalance(double balance) {
 		this.balance += balance;
 	}
+	//Method to remove Balance, also referred in Client
 	public void removeBalance(double balance) {
 		this.balance -= balance;
 	}
-
+	//Construcot and Getter
 	public Account(double balance) {
 		this.balance = balance;
 	}
